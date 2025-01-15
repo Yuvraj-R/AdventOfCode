f606adc84a3d6b1f105491211dc47ecb29d3d608
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-15 04:26:02 -0500

    completed day 2 part 2

--- 2/script.py
+++ 2/script.py
@@ -1,18 +1,36 @@
 ### PART 1 ###
+def strict_is_safe(nums):
+    if nums[1] > nums[0]:
+        return all(nums[i - 1] < nums[i] < nums[i - 1] + 4 for i in range(1, len(nums)))
+    else:
+        return all(nums[i - 1] > nums[i] > nums[i - 1] - 4 for i in range(1, len(nums)))
+
+
 count = 0
 with open("input.txt", "r") as file:
     for line in file:
         nums = [int(x) for x in line.split()]
-        valid = 1
-        if nums[1] > nums[0]:
-            if all(
-                nums[i - 1] < nums[i] < nums[i - 1] + 4 for i in range(1, len(nums))
-            ):
-                count += 1
-        else:
-            if all(
-                nums[i - 1] > nums[i] > nums[i - 1] - 4 for i in range(1, len(nums))
-            ):
-                count += 1
+        if strict_is_safe(nums):
+            count += 1
+
+print(count)
+
+
+### PART 2 ###
+def is_safe(nums):
+    if not strict_is_safe(nums):
+        for i in range(0, len(nums)):
+            if strict_is_safe(nums[:i] + nums[i + 1 :]):
+                return True
+        return False
+    return True
+
+
+count = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        nums = [int(x) for x in line.split()]
+        if is_safe(nums):
+            count += 1
 
 print(count)
