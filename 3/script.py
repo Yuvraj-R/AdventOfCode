8f6bda263b46e5c52b561e846427cf0a7318947a
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-16 00:45:56 -0500

    Completed day 3 part 2

--- 3/script.py
+++ 3/script.py
@@ -8,3 +8,17 @@ with open("input.txt", "r") as file:
 print(sum(int(match[0]) * int(match[1]) for match in matches))
 
 ### PART 2 ###
+
+with open("input.txt", "r") as file:
+    matches = re.findall(r"mul\(([0-9]+),([0-9]+)\)|(do\(\))|(don't\(\))", file.read())
+
+enabled, sum = True, 0
+for match in matches:
+    if match[2]:
+        enabled = True
+    elif match[3]:
+        enabled = False
+    elif enabled:
+        sum += int(match[0]) * int(match[1])
+
+print(sum)
