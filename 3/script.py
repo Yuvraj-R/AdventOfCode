29078bf6b0c2a86963d3b0787db6d00a4354a632
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-16 00:36:16 -0500

    completed day 3 part 1

---
+++ 3/script.py
@@ -0,0 +1,6 @@
+import re
+
+with open("input.txt", "r") as file:
+    matches = re.findall(r"mul\(([0-9]+),([0-9]+)\)", file.read())
+
+print(sum(int(match[0]) * int(match[1]) for match in matches))
