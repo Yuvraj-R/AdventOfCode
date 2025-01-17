61d5800f3e3d44ad42369f9a6256d4d3636efcb9
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-17 00:21:52 -0500

    Completed day 6 part 2

--- 6/script.py
+++ 6/script.py
@@ -22,3 +22,37 @@ matrix[pos[0]][pos[1]] = "X"
 
 total_moves = sum(line.count("X") for line in matrix)
 print(total_moves)
+
+### PART 2 ###
+with open("input.txt", "r") as file:
+    matrix = [list(line.strip()) for line in file]
+
+
+def is_loop(matrix, start_pos, offset):
+    visited = set()
+
+    while 0 <= start_pos[0] + offset[0] < len(matrix) and 0 <= start_pos[1] + offset[1] < len(matrix[0]):
+        state = (start_pos[0], start_pos[1], offset)
+        if state in visited:
+            return True
+        visited.add(state)
+
+        if matrix[start_pos[0] + offset[0]][start_pos[1] + offset[1]] != "#":
+            start_pos = (start_pos[0] + offset[0], start_pos[1] + offset[1])
+        else:
+            offset = mappings[offset]
+
+    return False
+
+
+total_positions = 0
+
+for r, row in enumerate(matrix):
+    for c, col in enumerate(row):
+        if col == ".":
+            matrix[r][c] = "#"
+            if is_loop(matrix, [53, 89], (-1, 0)):
+                total_positions += 1
+            matrix[r][c] = "."
+
+print(total_positions)
