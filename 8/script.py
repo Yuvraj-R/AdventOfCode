4f618f0b83ec49710469a3c41c763b80c51d5670
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-17 18:02:34 -0500

    Updated day 8 part 1

--- 8/script.py
+++ 8/script.py
@@ -2,40 +2,37 @@
 from collections import defaultdict
 
 with open("input.txt", "r") as file:
-    lines = file.readlines()
+    lines = [line.strip() for line in file]
 
-dim_y, dim_x = len(lines), len(lines[0].strip())
+dim_y, dim_x = len(lines), len(lines[0])
 antennas = defaultdict(list)
+
 for i, line in enumerate(lines):
-    for j, char in enumerate(line.strip()):
+    for j, char in enumerate(line):
         if char != ".":
             antennas[char].append((i, j))
 
 
-def is_valid_coordinate(coordinate):
-    return (0 <= coordinate[0] < dim_y) and (0 <= coordinate[1] < dim_x)
+def is_valid_coordinate(y, x):
+    return 0 <= y < dim_y and 0 <= x < dim_x
 
 
 def generate_antinodes(coord1, coord2):
-    dy, dx = coord1[1] - coord2[1], coord1[0] - coord2[0]
-    possible_antinodes = [
-        (coord1[0]+dx, coord1[1]+dy),
-        (coord2[0]-dx, coord2[1]-dy)
+    dy, dx = coord2[0] - coord1[0], coord2[1] - coord1[1]
+    return [
+        (y, x) for y, x in [
+            (coord1[0] - dx, coord1[1] + dy),
+            (coord2[0] + dx, coord2[1] - dy)
+        ] if is_valid_coordinate(y, x)
     ]
-    result = []
-    for c in possible_antinodes:
-        if is_valid_coordinate(c):
-            result.append(c)
-
-    return result
 
 
-antinode_coordinates = set()
-for antenna, coordinates in antennas.items():
-    for i in range(len(coordinates)):
-        for j in range(i+1, len(coordinates)):
-            antinode_coordinates.update(
-                generate_antinodes(coordinates[i], coordinates[j])
-            )
+antinode_coordinates = {
+    antinode
+    for coordinates in antennas.values()
+    for i, coord1 in enumerate(coordinates)
+    for coord2 in coordinates[i+1:]
+    for antinode in generate_antinodes(coord1, coord2)
+}
 
 print(len(antinode_coordinates))
