938f4ae554b0b848486f9a91954e5fdd642771f6
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-16 20:10:07 -0500

    Completed day 5 part 2

--- 5/script.py
+++ 5/script.py
@@ -15,11 +15,15 @@ for key, value in rules:
     rules_map[key].add(value)
 
 
-# validate each update and keep a running sum
+# validate each update and keep track of running sum and invalid updates
+invalid_updates = []  # for part 2
+
+
 def validate_update(update):
     seen = set()
     for page in update:
         if any(successor in seen for successor in rules_map[page]):
+            invalid_updates.append(set(update))
             return 0
         seen.add(page)
 
@@ -28,3 +32,19 @@ def validate_update(update):
 
 total_sum = sum(validate_update(update) for update in updates)
 print(total_sum)
+
+
+### PART 2 ###
+def correct_update(update):
+    result = []
+
+    for page in update:
+        count = sum(1 if successor in update else 0 for successor in rules_map[page])
+        result.append((page, count))
+
+    result.sort(key=lambda x: x[1])
+    return result[len(result) // 2][0]
+
+
+total_corrected_sum = sum(correct_update(update) for update in invalid_updates)
+print(total_corrected_sum)
