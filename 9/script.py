b5784e77fa2372c077ad2156c0c9005cf4fdcca2
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-18 02:14:09 -0500

    Updated day 9 part 1

--- 9/script.py
+++ 9/script.py
@@ -3,36 +3,29 @@
 import sys
 
 sys.set_int_max_str_digits(20000)
+
 with open("input.txt", "r") as file:
     nums = [int(num) for num in file.readline().strip()]
 
-ordering, id, is_file = [], 0, True
-for i in range(0, len(nums), 1):
+ordering, is_file, id = [], True, 0
+for num in nums:
     if is_file:
-        for j in range(0, nums[i]):
-            ordering.append(id)
-    else:
+        ordering.extend([id] * num)
         id += 1
-        for k in range(0, nums[i]):
-            ordering.append(".")
+    else:
+        ordering.extend(["."] * num)
     is_file = not is_file
 
 left, right = 0, len(ordering)-1
 while left < right:
-    while ordering[left] != ".":
+    while left < right and ordering[left] != ".":
         left += 1
-    while ordering[right] == ".":
+    while left < right and ordering[right] == ".":
         right -= 1
+    if left < right:
+        ordering[left], ordering[right] = ordering[right], ordering[left]
 
-    ordering[left], ordering[right] = ordering[right], ordering[left]
-
-ordering[left], ordering[right] = ordering[right], ordering[left]
-
-left, checksum = 0, 0
-while ordering[left] != ".":
-    checksum += (left * ordering[left])
-    left += 1
-
+checksum = sum(idx * num for idx, num in enumerate(ordering) if num != ".")
 print(checksum)
 
 ### PART 2 ###
