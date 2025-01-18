934ebbe64234bdaf523a3e995ee4d3e759a363db
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-18 16:14:36 -0500

    Updated day 10 part 1

--- 10/script.py
+++ 10/script.py
@@ -11,23 +11,23 @@ def in_bounds(y, x):
 
 
 def dfs(y, x, found):
-    if not (0 <= y < num_rows and 0 <= x < num_cols):
+    if (y, x) in found or not in_bounds(y, x):
         return 0
 
     cur = topography[y][x]
-
-    if cur == 9 and (y, x) not in found:
+    if cur == 9:
         found.add((y, x))
         return 1
 
     # Up Down Left Right coordinates
     nearby = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
+    return sum(dfs(ny, nx, found) for ny, nx in nearby if in_bounds(ny, nx) and topography[ny][nx] == cur + 1)
 
-    return sum(dfs(c[0], c[1], found) for c in nearby if in_bounds(c[0], c[1]) and topography[c[0]][c[1]] == cur+1)
 
-
-total_score = sum(dfs(y, x, set())
-                  for y, y_val in enumerate(topography) for x, x_val in enumerate(y_val) if x_val == 0)
+total_score = sum(
+    dfs(y, x, set())
+    for y, row in enumerate(topography) for x, val in enumerate(row) if val == 0
+)
 print(total_score)
 
 ### PART 2 ###
