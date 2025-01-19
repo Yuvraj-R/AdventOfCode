52ae517309bcf61b732d0cc7b9c9e76a948fa8b6
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-19 16:23:03 -0500

    Updated day 12 part 1

--- 12/script.py
+++ 12/script.py
@@ -2,41 +2,42 @@
 with open("input.txt", "r") as file:
     matrix = [list(line.strip()) for line in file.readlines()]
 
-coordinates = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+visited = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
 
 
 def in_bounds(y, x):
-    return 0 <= y < len(coordinates) and 0 <= x < len(coordinates[0])
+    """Check if the given coordinates are within matrix bounds."""
+    return 0 <= y < len(matrix) and 0 <= x < len(matrix[0])
 
 
-def dfs(y, x, last):
-    if not in_bounds(y, x) or matrix[y][x] != last:
+def dfs(y, x, plant_type):
+    if not in_bounds(y, x) or matrix[y][x] != plant_type:
         return (0, 1)
-    elif coordinates[y][x] == 1:
+    if visited[y][x] == 1:
         return (0, 0)
 
-    coordinates[y][x] = 1
+    visited[y][x] = 1
+
     area, perimeter = 1, 0
 
-    adjacents = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
-    for adj_y, adj_x in adjacents:
-        next_area, next_perimeter = dfs(adj_y, adj_x, matrix[y][x])
+    for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        next_area, next_perimeter = dfs(y + dy, x + dx, plant_type)
         area += next_area
         perimeter += next_perimeter
 
     return (area, perimeter)
 
 
-def processRegion(y, x):
-    """ Returns the price for fencing around a region """
+def calculate_region_cost(y, x):
+    """ Calculate the fencing cost for a region starting at (y, x). """
     area, perimeter = dfs(y, x, matrix[y][x])
     return area * perimeter
 
 
-total_price = 0
-for i, row in enumerate(coordinates):
-    for j, val in enumerate(row):
-        if val == 0:
-            total_price += processRegion(i, j)
+total_cost = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if visited[i][j] == 0:
+            total_cost += calculate_region_cost(i, j)
 
-print(total_price)
+print(total_cost)
