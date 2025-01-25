5a852df6f1d9ca270f9baf435a74d3e3073b5d79
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-01-25 13:30:06 -0500

    Updated day 19 part 2

--- 19/script.py
+++ 19/script.py
@@ -7,25 +7,20 @@ class Day19:
         patterns = lines[0].split(", ")
 
         def backtrack(index, design, memo):
-            # Check if the result for this index is already memoized
             if index in memo:
                 return memo[index]
 
-            # Base case: reached the end of the design
             if index == len(design):
                 return 1
 
             res = 0
-            # Try matching each pattern at the current index
             for pattern in patterns:
-                if design.startswith(pattern, index):  # Pattern matches the substring
+                if design.startswith(pattern, index):
                     res += backtrack(index + len(pattern), design, memo)
 
-            # Memoize the result for the current index
             memo[index] = res
             return res
 
-        # Sum the results for all designs
         total_ways = 0
         for towel in lines[1:]:
             total_ways += backtrack(0, towel, {})
